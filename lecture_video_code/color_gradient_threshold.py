723ec506a0f1a9b8b4f1625c9cc7a5567dfd3feb
sreakdgeek <srikanth.vid@gmail.com>
2017-12-11 04:07:57 -0500

    First commit

---
+++ lecture_video_code/color_gradient_threshold.py
@@ -0,0 +1,54 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+# Edit this function to create your own pipeline.
+def pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
+    img = np.copy(img)
+    # Convert to HLS color space and separate the V channel
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(np.float)
+    l_channel = hls[:,:,1]
+    s_channel = hls[:,:,2]
+    # Sobel x
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0) # Take the derivative in x
+    abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+    
+    # Threshold x gradient
+    sxbinary = np.zeros_like(scaled_sobel)
+    sxbinary[(scaled_sobel > sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
+    
+    # Threshold color channel
+    s_binary = np.zeros_like(l_channel)
+    s_binary[(l_channel > s_thresh[0]) & (l_channel <= s_thresh[1])] = 1
+    
+    print("s_binary = " + str(sum(sum(s_binary))))
+    print("sxbinary = " + str(sum(sum(sxbinary))))
+
+    # Stack each channel
+    # Note color_binary[:, :, 0] is all 0s, effectively an all black image. It might
+    # be beneficial to replace this channel with something else.
+    color_binary = np.dstack(( sxbinary, sxbinary, s_binary)) * 255
+    combined_binary = np.zeros_like(sxbinary)
+    combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
+    plt.imshow(color_binary)
+    plt.show()
+    return color_binary
+
+image = mpimg.imread('bridge_shadow.jpg')
+
+result = pipeline(image)
+
+# Plot the result
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=40)
+
+ax2.imshow(result)
+ax2.set_title('Pipeline Result', fontsize=40)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
